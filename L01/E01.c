68641fd4d2163d4ca4eca3c80ee3cd1c16757f65
Davide Fassio <55668928+Davidefassio@users.noreply.github.com>
2020-09-29 09:36:58 +0200

    Update E01.c

--- L01/E01.c
+++ L01/E01.c
@@ -1,1 +1,42 @@
-ciao
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+    const int n = 15;
+    int vec[n] = {1, 3, 4, 0, 1, 0, 9, 4, 2, 0, 1, 2, 3};
+    int max = 0, curr = 0;
+    int i, j;
+    
+    for(i = 0; i < n; ++i){
+        if(vec[i] != 0){ ++curr; }
+        else{
+            if(curr > max){
+                max = curr;
+            }
+            curr = 0;
+        }
+    }
+    if(curr > max){
+        max = curr;
+    }
+    
+    for(i = 0; i < n; ++i){
+        if(vec[i] != 0){
+            for(j = i; j < i + max; ++j){
+                if(vec[j] == 0){
+                    i = j;
+                    break;
+                }
+            }
+            if(j == i + max){
+                printf("[ ");
+                for(j = i; j < i + max; ++j){
+                    printf("%d ", vec[j]);
+                }
+                printf("]\n");
+            }
+        }
+    }
+    
+    return 0;
+}
