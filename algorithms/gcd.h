3ce68e08cb53c013f61a3b8e1b183070b4d556ff
Davide Fassio <55668928+Davidefassio@users.noreply.github.com>
2020-10-19 11:35:32 +0200

    Update gcd.h

--- algorithms/gcd.h
+++ algorithms/gcd.h
@@ -1,1 +1,14 @@
+#ifndef GCD_H
+#define GCD_H
 
+/*
+ * Compute the greatest common divisor of two positive integers
+ * using the euclidean algorithm.
+ */
+int gcd(int a, int b){
+  if(b == 0){ return(a); }
+  else{ return gcd(b, a%b); }
+}
+
+
+#endif /* GCD_H */
