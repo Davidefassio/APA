13e6af76f40a5beff7420a6690bbc19c561b4052
Davide Fassio <55668928+Davidefassio@users.noreply.github.com>
2020-10-19 11:32:29 +0200

    Create E01.c

---
+++ L04/E01/E01.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int gcd(int a, int b){
+    if(a == b){ return a; } // Terminazione
+    
+    if(b > a){ // Scambio
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+    
+    if(a%2 == 0){
+        if(b%2 == 0)
+            return 2*gcd(a/2, b/2);
+        return gcd(a/2, b);
+    }
+    if(b%2 == 0){
+        return gcd(a, b/2);
+    }
+    return gcd((a-b)/2, b);
+    
+}
+
+int main(int argc, char *argv[]){
+    int x, y;
+    printf("Inserisci due numeri interi positivi:\n");
+    scanf("%d %d", &x, &y);
+    
+    printf("GCD: %d\n", gcd(x, y));
+    
+    return 0;
+}
