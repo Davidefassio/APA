84ffec4402289566d9659c49c020b47fa5b13bea
Davide Fassio <55668928+Davidefassio@users.noreply.github.com>
2020-10-21 22:36:17 +0200

    Update E02.c

--- L03/E02/E02.c
+++ L03/E02/E02.c
@@ -5,11 +5,11 @@
 void stampaCodifica(void *p, int size, int bigEndian){
     unsigned char *c;
     int i, j;
-    
+
     if(size > 8){ size = 10; } // Long double utilizzano 10 Byte
-    
+
     if(bigEndian){  // Stampo in ordine crescente di Byte
-        c = (unsigned char*) p;
+        c = ((unsigned char*) p);
         printf("Sign: ");
         for(i = 0; i < size; ++i, ++c){
             for(j = 7; j > -1; --j){
@@ -39,7 +39,7 @@ void stampaCodifica(void *p, int size, int bigEndian){
 
 int main(int argc, char *argv[]){
     int bigEndian;
-    
+
     int b = 1;
     if(*((unsigned char*) &b) & 0x01){
         bigEndian = 0;
@@ -49,26 +49,25 @@ int main(int argc, char *argv[]){
         bigEndian = 1;
         printf("L'architettura e' big endian\n\n");
     }
-    
+
     float af;
-    double ad;
+    double ad, tmp;
     long double ald;
-    
+
     printf("Sizeof float: %lu bit = %lu Byte\n", 8*sizeof(float), sizeof(float));
     printf("Sizeof double: %lu bit = %lu Byte\n", 8*sizeof(double), sizeof(double));
     printf("Sizeof long double: %d bit = %d Byte\n\n", 80, 10);
-    
-    printf("Inserire numero float: ");
-    scanf("%f", &af);
+
+    printf("Inserire numero: ");
+    scanf("%lg", &tmp);
+
+    af = (float) tmp;
+    ad = tmp;
+    ald = (long double) tmp;
+
     stampaCodifica((void*) &af, sizeof(float), bigEndian);
-    
-    printf("Inserire numero double: ");
-    scanf("%lg", &ad);
     stampaCodifica((void*) &ad, sizeof(double), bigEndian);
-    
-    printf("Inserire numero long double: ");
-    scanf("%Lf", &ald);
     stampaCodifica((void*) &ald, sizeof(long double), bigEndian);
-    
+
     return 0;
 }
