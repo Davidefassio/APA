b8933aceb87830dbc876f751f4d97604fa999c38
Davide Fassio <55668928+Davidefassio@users.noreply.github.com>
2020-11-10 21:56:59 +0100

    Update E02.c

--- L07/E02/E02.c
+++ L07/E02/E02.c
@@ -1,1 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "list.h"
 
+int main(int argc, char *argv[]){
+    char nome_file[20];
+
+    printf("Inserire nome file: ");
+    scanf("%s", nome_file);
+
+    FILE *f = fopen(nome_file, "r");
+    if(f == NULL) exit(EXIT_FAILURE);
+
+    
+
+
+    return 0;
+}
