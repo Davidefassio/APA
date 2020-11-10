b730cf8b2ea7f9a5f77a4e56a7bb0beee15d9074
Davide Fassio <55668928+Davidefassio@users.noreply.github.com>
2020-11-10 12:03:57 +0100

    Update gcd.h

--- Algorithms/gcd.h
+++ Algorithms/gcd.h
@@ -6,7 +6,13 @@
  * using the euclidean algorithm.
  * Important: a > b
  */
-int gcd(int a, int b){
+
+int gcd(int a, int b){ // Wrapper
+  if(a > b) return gcdR(a, b);
+  return gcdR(b, a);
+}
+
+int gcdR(int a, int b){ // Recursion
   if(!b) return(a);
   return gcd(b, a%b);
 }
