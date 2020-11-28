0dafe97180d93410bf981487cc4e8a1aa6b8943e
Davide Fassio <55668928+Davidefassio@users.noreply.github.com>
2020-11-28 18:13:52 +0100

    Update inventario.h

--- L09/E03/inventario.h
+++ L09/E03/inventario.h
@@ -1,6 +1,31 @@
 #ifndef INVENTARIO_H_INCLUDED
 #define INVENTARIO_H_INCLUDED
 
+#define MAXL 50 // Lunghezza massima delle stringhe
 
+// Sestupla delle statistiche.
+typedef struct{
+    int hp;
+    int mp;
+    int atk;
+    int def;
+    int mag;
+    int spr;
+}stat_t;
 
-#endif // INVENTARIO_H_INCLUDED
+// Struct contenente un oggetto.
+typedef struct{
+    char nome[MAXL+1];
+    char tipo[MAXL+1];
+    stat_t stat;
+}inv_t;
+
+// Vector di inv_t, cioe' la tabella degli oggetti.
+typedef struct{
+    inv_t *vettInv;
+    int nInv;
+}tabInv_t;
+
+// Prototipi funzioni.
+
+#endif /* INVENTARIO_H_INCLUDED */
