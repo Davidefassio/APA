24973394e02d55a2db9562819d8cd70e9b860dfb
Davide Fassio <55668928+Davidefassio@users.noreply.github.com>
2020-12-12 22:00:39 +0100

    Update E01.c

--- L10/E01/E01.c
+++ L10/E01/E01.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 
 // Struttura delle attività
 // Inizio, fine, durata
@@ -8,6 +9,9 @@ typedef struct{
 }Att;
 
 void attSel(Att*, int, int*);
+int cmp_att(const void*, const void*);
+int compatible(Att, Att);
+void printSeq(Att*, int*, int, int);
 
 int main(int argc, char *argv[]){
     int i, nr;
@@ -41,11 +45,72 @@ int main(int argc, char *argv[]){
 }
 
 void attSel(Att *att, int n, int *sol){
+    // Ordino per ordine crescente dei tempi di fine il vettore delle attivita
+    qsort(att, n, sizeof(Att), cmp_att);
+    
     // Dichiaro le variabili
-    int i, j, cnt = 0;
-    int *tmp = (int*) malloc(n * sizeof(int));
+    int i, j, max, imax, lmax = INT_MIN, limax = -1, flag;
+    int *len = (int*) malloc(n * sizeof(int));
 
     // Algoritmo
-
+    len[0] = att[0].dur;
+    sol[0] = -1;
+    
+    for(i = 1; i < n; ++i){
+        flag = 1;
+        max = INT_MIN;
+        imax = -1;
+        
+        // Compatibilita indietro e ricerca della sequenza ottimale
+        for(j = i-1; j >= 0; --j){
+            if(compatible(att[i], att[j]) && len[j] + att[i].dur > max){
+                flag = 0;
+                max = len[j] + att[i].dur;
+                imax = j;
+            }
+        }
+        
+        // Trovata compatibilita o meno
+        if(flag){
+            len[i] = att[i].dur;
+            sol[i] = -1;
+        }
+        else{
+            len[i] = max + att[i].dur;
+            sol[i] = imax;
+        }
+        
+        // Aggiornamento della sequenza massima
+        if(len[i] > lmax){
+            lmax = len[i];
+            limax = i;
+        }
+    }
+    
     // Stampa
+    printf("Durata massima: %d\n", lmax);
+    printSeq(att, sol, n, limax);
+    
+    // Free
+    free(len);
+}
+
+void printSeq(Att *att, int *sol, int n, int index){
+    if(index == -1) return;
+    printSeq(att, sol, n, sol[index]);
+    printf("(%d, %d)\n", att[index].sta, att[index].fin);
+    return;
+}
+
+// Ordine crescente sul tempo finale delle attivita
+int cmp_att(const void *p1, const void *p2){
+    int t1 = (*(const Att*)p1).sta;
+    int t2 = (*(const Att*)p2).sta;
+    return (t1 > t2) - (t1 < t2);
+}
+
+int compatible(Att a1, Att a2){
+    if(a1.sta < a2.fin && a1.fin > a2.sta)
+        return 0; // Non compatibili
+    return 1; // Compatibili
 }
