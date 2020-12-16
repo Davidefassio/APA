6799bfff77693ffe5055ee194c7e7363e3330534
Davide Fassio <55668928+Davidefassio@users.noreply.github.com>
2020-12-17 00:25:07 +0100

    Update E02.c

--- L10/E02/E02.c
+++ L10/E02/E02.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 #include "tensor.h"
 
-int creaCollana(int*, int*);
-void printCollana(int*, int);
+int creaCollana(int*);
+int fz(int*, Tensor5d);
+int fr(int*, Tensor5d);
+int ft(int*, Tensor5d);
+int fs(int*, Tensor5d);
 
 int main(int argc, char * argv[]){
     int i, nr, len;
-    int gems[4], *sol = NULL;
-
-    Tensor5d mem;
-    mem = TensorInit(4, 1, 1, 1, 1);
+    int gems[4];
 
     FILE *fp;
     char nome_file[30];
@@ -25,51 +25,125 @@ int main(int argc, char * argv[]){
     for(i = 0; i < nr; ++i){ // Ciclo su ogni collana
         fscanf(fp, "%d %d %d %d", &gems[0], &gems[1], &gems[2], &gems[3]);
 
-        // Lunghezza massima collana
-        len = gems[0] + gems[1] + gems[2] + gems[3];
-        // Vettore riallocato dinamicamente della soluzione
-        sol = (int*) realloc(sol, len * sizeof(int));
-
-        // Crea una collana e salvala in sol
-        len = creaCollana(gems, sol);
-
-        printCollana(sol, len);
+        // Crea una collana e restituisci la lunghezza massima
+        len = creaCollana(gems);
+        
+        printf("%2d) %d\n", i+1, len);
     }
     fclose(fp);
 
-    // Free
-    free(sol);
-    TensorFree(mem);
-
     return 0;
 }
 
-void printCollana(int *v, int n){
-    int i;
-    printf("%d: ", n);
-    for(i = 0; i < n; ++i){
-        switch(v[i]){
-        case 0:
-            printf("Z ");
-            break;
-        case 1:
-            printf("R ");
-            break;
-        case 2:
-            printf("T ");
-            break;
-        case 3:
-            printf("S ");
-            break;
-        default:
-            exit(EXIT_FAILURE);
-            break;
-        }
-    }
-    printf("\n");
+int creaCollana(int *gems){
+    Tensor5d mem = TensorInit(4, gems[0], gems[1], gems[2], gems[3]);
+    
+    int max = -1, n = 0;
+    
+    n = fz(gems, mem);
+    if(n > max) max = n;
+    
+    n = fr(gems, mem);
+    if(n > max) max = n;
+    
+    n = ft(gems, mem);
+    if(n > max) max = n;
+    
+    n = fs(gems, mem);
+    if(n > max) max = n;
+    
+    // TensorFree(mem);
+    return max;
 }
 
-int creaCollana(int *gems, int *sol){
-    // TODO
-    return 0;
+int fz(int *gems, Tensor5d t){
+    // Ho finito le gemme
+    if(gems[0] == 0)
+        return 0;
+    
+    // Conosco gia la posiziones
+    if(t.ten[0][gems[0]][gems[1]][gems[2]][gems[3]] != -1)
+        return t.ten[0][gems[0]][gems[1]][gems[2]][gems[3]];
+    
+    --gems[0]; // Metto una pietra
+    
+    int max = fz(gems, t), n = fr(gems, t);
+    if(n > max) max = n;
+    ++max;
+    
+    ++gems[0];
+    
+    // Salvo il risultato per la memoization
+    t.ten[0][gems[0]][gems[1]][gems[2]][gems[3]] = max;
+    
+    return max;
+}
+
+int fr(int *gems, Tensor5d t){
+    // Ho finito le gemme
+    if(gems[1] == 0)
+        return 0;
+    
+    // Conosco gia la posizione
+    if(t.ten[1][gems[0]][gems[1]][gems[2]][gems[3]] != -1)
+        return t.ten[1][gems[0]][gems[1]][gems[2]][gems[3]];
+    
+    --gems[1]; // Metto una pietra
+    
+    int max = ft(gems, t), n = fs(gems, t);
+    if(n > max) max = n;
+    ++max;
+    
+    ++gems[1];
+    
+    // Salvo il risultato per la memoization
+    t.ten[1][gems[0]][gems[1]][gems[2]][gems[3]] = max;
+    
+    return max;
+}
+
+int ft(int *gems, Tensor5d t){
+    // Ho finito le gemme
+    if(gems[2] == 0)
+        return 0;
+    
+    // Conosco gia la posizione
+    if(t.ten[2][gems[0]][gems[1]][gems[2]][gems[3]] != -1)
+        return t.ten[2][gems[0]][gems[1]][gems[2]][gems[3]];
+    
+    --gems[2]; // Metto una pietra
+    
+    int max = fz(gems, t), n = fr(gems, t);
+    if(n > max) max = n;
+    ++max;
+    
+    ++gems[2];
+    
+    // Salvo il risultato per la memoization
+    t.ten[2][gems[0]][gems[1]][gems[2]][gems[3]] = max;
+    
+    return max;
+}
+
+int fs(int *gems, Tensor5d t){
+    // Ho finito le gemme
+    if(gems[3] == 0)
+        return 0;
+    
+    // Conosco gia la posizione
+    if(t.ten[3][gems[0]][gems[1]][gems[2]][gems[3]] != -1)
+        return t.ten[3][gems[0]][gems[1]][gems[2]][gems[3]];
+    
+    --gems[3]; // Metto una pietra
+    
+    int max = ft(gems, t), n = fs(gems, t);
+    if(n > max) max = n;
+    ++max;
+    
+    ++gems[3];
+    
+    // Salvo il risultato per la memoization
+    t.ten[3][gems[0]][gems[1]][gems[2]][gems[3]] = max;
+    
+    return max;
 }
