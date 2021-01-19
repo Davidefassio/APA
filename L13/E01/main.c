bfad2a9846cb58b8ddb9df11f37c38d8644cdb99
Davide Fassio <55668928+Davidefassio@users.noreply.github.com>
2021-01-20 00:01:00 +0100

    Update main.c

--- L13/E01/main.c
+++ L13/E01/main.c
@@ -3,6 +3,18 @@
 #include "grafo.h"
 
 int main(int argc, char *argv[]){
+    char nome_file[31];
+    FILE *fp;
+
+    printf("Inserisci nome file: ");
+    scanf("%s", nome_file);
+
+    fp = fopen(nome_file, "r");
+    if(fp == NULL) exit(EXIT_FAILURE);
+
+    g = GRF_init(fp);
+
+    fclose(fp);
 
     return 0;
 }
